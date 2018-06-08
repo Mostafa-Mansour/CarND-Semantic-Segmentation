d017ff5f72b1a68318f43627f8dc20a6572df247
Mostafa Mansour <mansour@ws-11429-mbp.intra.tut.fi>
2018-06-08 18:14:41 +0300

    Semantic Segmentaion project

--- README.md
+++ README.md
@@ -12,35 +12,20 @@ Make sure you have the following is installed:
 ##### Dataset
 Download the [Kitti Road dataset](http://www.cvlibs.net/datasets/kitti/eval_road.php) from [here](http://www.cvlibs.net/download.php?file=data_road.zip).  Extract the dataset in the `data` folder.  This will create the folder `data_road` with all the training a test images.
 
-### Start
-##### Implement
-Implement the code in the `main.py` module indicated by the "TODO" comments.
-The comments indicated with "OPTIONAL" tag are not required to complete.
-##### Run
-Run the following command to run the project:
-```
-python main.py
-```
-**Note** If running this in Jupyter Notebook system messages, such as those regarding test status, may appear in the terminal rather than the notebook.
+## Implementation
+1. The following steps are followed to implement Fully Connected Network (FCN):
+* The weights and the model of VGG16 are restored.
+* The outputs of the interesting layers (layer_3,layer_4,layer_7) are saved to be used later.
+* A 1X1 convolution has been implemented to the output of the interesting layers.
+* Layer_7 was upsampled using a stride of (2,2) ---> upsampled_layer_7
+* the upsampled_layer_7 was fused with layer_4 ----> fused_7_4
+* the fused_7_4 was upsampled with stride of (2,2) --> upsampled_7_4
+* The upsampled_7_4 was fused with the output of layer_3 ---> fused_7_4_3
+* The output of fused_7_4_3 was upsampled with stride of (8,8) to get the final output (semantic segmented image)
 
-### Submission
-1. Ensure you've passed all the unit tests.
-2. Ensure you pass all points on [the rubric](https://review.udacity.com/#!/rubrics/989/view).
-3. Submit the following in a zip file.
- - `helper.py`
- - `main.py`
- - `project_tests.py`
- - Newest inference images from `runs` folder  (**all images from the most recent run**)
- 
- ### Tips
-- The link for the frozen `VGG16` model is hardcoded into `helper.py`.  The model can be found [here](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip)
-- The model is not vanilla `VGG16`, but a fully convolutional version, which already contains the 1x1 convolutions to replace the fully connected layers. Please see this [forum post](https://discussions.udacity.com/t/here-is-some-advice-and-clarifications-about-the-semantic-segmentation-project/403100/8?u=subodh.malgonde) for more information.  A summary of additional points, follow. 
-- The original FCN-8s was trained in stages. The authors later uploaded a version that was trained all at once to their GitHub repo.  The version in the GitHub repo has one important difference: The outputs of pooling layers 3 and 4 are scaled before they are fed into the 1x1 convolutions.  As a result, some students have found that the model learns much better with the scaling layers included. The model may not converge substantially faster, but may reach a higher IoU and accuracy. 
-- When adding l2-regularization, setting a regularizer in the arguments of the `tf.layers` is not enough. Regularization loss terms must be manually added to your loss function. otherwise regularization is not implemented.
- 
-### Using GitHub and Creating Effective READMEs
-If you are unfamiliar with GitHub , Udacity has a brief [GitHub tutorial](http://blog.udacity.com/2015/06/a-beginners-git-github-tutorial.html) to get you started. Udacity also provides a more detailed free [course on git and GitHub](https://www.udacity.com/course/how-to-use-git-and-github--ud775).
+2. The Network was trained using Adam optimizer with softmax cross entropy cost function.
 
-To learn about REAMDE files and Markdown, Udacity provides a free [course on READMEs](https://www.udacity.com/courses/ud777), as well. 
+3. The learning rate was 0.0009 and a dropout of 0.5 was used.
 
-GitHub also provides a [tutorial](https://guides.github.com/features/mastering-markdown/) about creating Markdown files.
+
+
--- main.py
+++ main.py
@@ -33,7 +33,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess,[vgg_tag],vgg_path)
+    graph=tf.get_default_graph()
+    vgg_input_layer=graph.get_tensor_by_name(vgg_input_tensor_name)
+    vgg_keep_prop=graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    vgg_layer3_out=graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    vgg_layer4_out=graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    vgg_layer7_out=graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return vgg_input_layer, vgg_keep_prop, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +56,27 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    conv_1x1_7_scores=tf.layers.conv2d(vgg_layer7_out,num_classes,1,padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    conv_1x1_4_scores=tf.layers.conv2d(vgg_layer4_out,num_classes,1,padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    conv_1x1_3_scores=tf.layers.conv2d(vgg_layer3_out,num_classes,1,padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    # layer_7 upsample
+    layer_7_scores_up=tf.layers.conv2d_transpose(conv_1x1_7_scores,num_classes,4,strides=(2,2),padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(0.5))
+    
+    #layer4 + layer7 fuse
+    layer_4_7_fused=tf.add(layer_7_scores_up,conv_1x1_4_scores)
+    
+    #upsample fused_7_4
+    layer_4_7_fused_up=tf.layers.conv2d_transpose(layer_4_7_fused,num_classes,4,strides=(2,2),padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(0.5))
+    
+    #fuse layer_4_7_up with layer 3
+    layer_4_7_3_fused=tf.add(layer_4_7_fused_up,conv_1x1_3_scores)
+    
+    #upsample
+    layer_4_7_3_fused_up=tf.layers.conv2d_transpose(layer_4_7_3_fused,num_classes,16,strides=(8,8),padding='same',kernel_regularizer=tf.contrib.layers.l2_regularizer(0.5))
+    
+    
+    return layer_4_7_3_fused_up
 tests.test_layers(layers)
 
 
@@ -61,7 +90,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1,num_classes))
+    # define loss function
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits, labels= correct_label))
+    # define training operation
+    optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -81,7 +118,18 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+    
+    print("Training...")
+    print()
+    for i in range(epochs):
+        print("EPOCH {} ...".format(i+1))
+        for image, label in get_batches_fn(batch_size):
+            _, loss = sess.run([train_op, cross_entropy_loss], 
+                               feed_dict={input_image: image, correct_label: label,keep_prob: 0.5, learning_rate: 0.0009})
+            print("Loss: = {:.3f}".format(loss))
+        print()
+
 tests.test_train_nn(train_nn)
 
 
@@ -100,6 +148,7 @@ def run():
     #  https://www.cityscapes-dataset.com/
 
     with tf.Session() as sess:
+        
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
@@ -107,12 +156,24 @@ def run():
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-
+        
         # TODO: Build NN using load_vgg, layers, and optimize function
+        epochs = 50
+        batch_size = 5
 
+        # TF placeholders
+        correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        
+        input_image,keep_prob,layer_3_out,layer_4_out,layer_7_out=load_vgg(sess, vgg_path)
+        layer_output=layers(layer_3_out,layer_4_out,layer_7_out,num_classes)
+        logits, train_op, cross_entropy_loss=optimize(layer_output,correct_label,learning_rate,num_classes)
+        
         # TODO: Train NN using the train_nn function
-
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate)
         # TODO: Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
